552a885831040995e4e0295e5fce517ded76ec73
unknown <charan7728264@gmail.com>
2023-07-05 17:30:47 +0530

    second

--- src/components/api.js
+++ src/components/api.js
@@ -5,7 +5,7 @@ export default function Client( ){
 const client = createClient('DnS6kIU35UjI6H608t9wBb6HN8ro2GFvK9msrGG0RoP97FdYHE07x4wC');
 const query = 'Nature';
 
-client.photos.search({ query, per_page: 500 }).then(photos => {console.log(photos)});
+client.photos.search({ query, per_page:500,page:8 }).then(photos => {console.log(photos)});
 
 return(
     <div>
