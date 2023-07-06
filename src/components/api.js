9bdad3e9e3beb2c87e6a72387f4c7002eccc2114
unknown <charan7728264@gmail.com>
2023-07-06 10:30:25 +0530

    second

--- src/components/api.js
+++ src/components/api.js
@@ -1,20 +1,36 @@
 import { createClient } from 'pexels';
+import { useState } from 'react';
 import Masonry from "react-masonry-css";
-
+import "../styles/api.css"
 export default function Client( ){
 const client = createClient('DnS6kIU35UjI6H608t9wBb6HN8ro2GFvK9msrGG0RoP97FdYHE07x4wC');
 const query = 'Nature';
+const [data,setData] = useState({photos:[]})
 
-client.photos.search({ query, per_page:500,page:8 }).then(photos => {console.log(photos)});
+client.photos.search({ query, per_page:500,page:8 }).then(res => {
+    console.log(res);
+    setData({photos:res.photos})
+});
+
+
+
 
 return(
     <Masonry
   breakpointCols={3}
   className="my-masonry-grid"
-  columnClassName="my-masonry-grid_column">
-    <div>
-        
-    </div>
+  columnClassName="my-masonry-grid_column"
+  columns={4}
+  >
+        {
+        data&& data.photos.map((e)=>{
+                return(
+                    <div>
+                        <img src={e.src.portrait}/>
+                    </div>
+                )
+            })
+        }
   </Masonry>
 )
 }
