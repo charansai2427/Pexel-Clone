2403f5fe4008e29476a5a614f99de2b318420bd7
unknown <charan7728264@gmail.com>
2023-07-06 09:50:20 +0530

    second

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,5 @@
-
 import { createClient } from 'pexels';
+import Masonry from "react-masonry-css";
 
 export default function Client( ){
 const client = createClient('DnS6kIU35UjI6H608t9wBb6HN8ro2GFvK9msrGG0RoP97FdYHE07x4wC');
@@ -8,8 +8,13 @@ const query = 'Nature';
 client.photos.search({ query, per_page:500,page:8 }).then(photos => {console.log(photos)});
 
 return(
+    <Masonry
+  breakpointCols={3}
+  className="my-masonry-grid"
+  columnClassName="my-masonry-grid_column">
     <div>
-
+        
     </div>
+  </Masonry>
 )
 }
