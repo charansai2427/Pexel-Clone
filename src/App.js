64b4d3c48124a5bcb29d59a7048161268169791a
charansai2427 <131942169+charansai2427@users.noreply.github.com>
2024-03-22 14:05:45 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Navbar from "./components/navbar";
 import Client from "./components/api";
 import { useState } from "react";
-
 function App() {
   const [ state,setState] = useState("nature")
-  
    return (
     <div className="App">
 <Navbar setState={setState}/>
